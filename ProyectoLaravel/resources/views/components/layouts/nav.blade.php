9c3c4e7331c093ae2a8d42882e4574153c452562
a92tena <albertotena_92@hotmail.com>
2024-05-05 15:27:10 +0200

    Nuevo Proyecto con nuevo env

--- ProyectoLaravel/resources/views/components/layouts/nav.blade.php
+++ ProyectoLaravel/resources/views/components/layouts/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="h-10v bg-nav flex flex-row justify-start items-center space-x-2 ">
     <a class= "btn  btn-primary " href="about">About</a>
     <a class= "btn  btn-primary " href="/">home</a>
-    <a class= "btn  btn-primary " href="alumnos.index">Alumnos</a>
+    <a class= "btn  btn-primary " href="{{route("alumnos.index")}}">Alumnos</a>
     @auth
     <a class= "btn  btn-primary " href="proyecto">Proyectos</a>
     @endauth
